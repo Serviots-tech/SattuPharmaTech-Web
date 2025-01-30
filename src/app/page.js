16c412dd8931612e82621dad62a4c9dcb0b90520
Alok pal <alokp@serviots.com>
2025-01-30 15:28:23 +0530

    change : page js

--- src/app/page.js
+++ src/app/page.js
@@ -6,40 +6,35 @@ import ContactForm from '../components/ContactForm';
 import MainImage from "../components/MainImage";
 import WhyChooseUs from "../components/WhyChooseUsSection";
 import { cardWithFirstImgValue, cardWithSecondImgValue } from './constants/constValues';
-import ContactForm from '../components/ContactForm'
+import ExpertiseSection from '../components/ExpertiseSection';
+
+
 export default function Home() {
-
-
-
 	return (
 		<div className="w-full">
 			< MainImage />
+
 			<ExpertiseSection />
 
-			<div className="mt-20 mb-20">
-        <WhyChooseUs  align={'left'}/>
-      </div>
-			<div className="mt-20 mb-20">
-				<WhyChooseUs />
-			</div>
+			<div className="flex ">
+				<div>
 
+				</div>
+			</div>
 			<CardWithImage cardImg={docImg?.src} cardValue={cardWithFirstImgValue} isReversed={false} />
 
 			<div className='mt-10'>
 				<CardWithImage cardImg={teamImg?.src} cardValue={cardWithSecondImgValue} isReversed={true} />
 			</div>
 
+			<div className="mt-20 mb-20">
+				<WhyChooseUs align={'left'} />
+			</div>
+
+
 			<ContactForm />
 
-      {/* Request Call */}
-      {/* <div>
-        <RequestCall />
-      </div> */}
-
-      {/* About Us */}
-      {/* <div><AboutUs /></div> */}
-      {/* <Footer/> */}
-    </div>
+		</div>
 
 	);
 }
