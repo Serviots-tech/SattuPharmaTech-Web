6cac0cbac972be3bc1e1019fe00257923c43473c
Alok pal <alokp@serviots.com>
2025-02-03 19:12:40 +0530

    feat: vision Mission

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,55 +1,30 @@
 "use client";
 import { CardWithImage } from "src/components/Cards";
 import docImg from "../../../public/assets/TeamAbout.png";
-import { cardWithFourthImgValue } from '../constants/constValues';
+import { cardWithFourthImgValue } from "../constants/constValues";
+import Image from "next/image";
+import youngConfident from "../../../public/assets/youngConfident.png";
+import Maskgroup from "../../../public/assets/Maskgroup.png";
+import VisionMission from "src/components/VisionMission";
 
 const About = () => {
   return (
     <>
-     <div className="pt-10 pb-10 px-6 sm:px-10 md:px-16 lg:px-20 xl:px-10 w-full  mx-auto">
-      <p className="text-3xl mb-10 font-semibold font-poppins text-center">About SatuPharmTech</p>
-      <CardWithImage cardImg={docImg?.src} cardValue={cardWithFourthImgValue} isReversed={false} />
+      <div className="pt-10 pb-10 px-6 sm:px-10 md:px-16 lg:px-20 xl:px-20 w-full  mx-auto">
+        <p className="text-3xl mb-10 font-semibold font-poppins text-center">
+          About SatuPharmTech
+        </p>
+        <CardWithImage
+          cardImg={docImg?.src}
+          cardValue={cardWithFourthImgValue}
+          isReversed={false}
+        />
 
-      <p className="text-3xl mb-10 font-semibold font-poppins text-left">Our Mission & Vision</p>
-      <div className="w-full max-w-6xl mx-auto py-12 px-4">
-      {/* Mission Section */}
-      <div className="flex flex-col md:flex-row items-center justify-between mb-16 bg-amber-50 rounded-3xl overflow-hidden">
-        <div className="p-8 md:w-1/2">
-          <h2 className="text-4xl font-bold mb-4">MISSION</h2>
-          <p className="text-xl leading-relaxed">
-            To help startups explore new opportunities in new markets and stay thriving!
-          </p>
-        </div>
-        <div className="md:w-1/2">
-          <div className="relative w-full aspect-square">
-            <img 
-              src="/api/placeholder/600/600"
-              alt="Team collaborating"
-              className="rounded-full object-cover"
-            />
-          </div>
-        </div>
-      </div>
+        <p className="text-3xl mb-10 font-semibold font-poppins text-left">
+          Our Mission & Vision
+        </p>
 
-      {/* Vision Section */}
-      <div className="flex flex-col-reverse md:flex-row items-center justify-between bg-sky-50 rounded-3xl overflow-hidden">
-        <div className="md:w-1/2">
-          <div className="relative w-full aspect-square">
-            <img 
-              src="/api/placeholder/600/600"
-              alt="Professional team"
-              className="rounded-full object-cover"
-            />
-          </div>
-        </div>
-        <div className="p-8 md:w-1/2">
-          <h2 className="text-4xl font-bold mb-4">VISION</h2>
-          <p className="text-xl leading-relaxed">
-            To become a leader that defines a new way to help businesses with 100% success surety.
-          </p>
-        </div>
-      </div>
-    </div>
+       <VisionMission youngConfident={youngConfident} Maskgroup={Maskgroup}/>
       </div>
     </>
   );
---
+++ src/components/VisionMission.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React from "react";
+
+const VisionMission = ({youngConfident,Maskgroup}) => {
+  return (
+    <>
+      <div className=" sm:w-[65%] relative items-center h-48 p-4 bg-[#FFF7E3] font-poppins">
+        <Image
+          src={youngConfident}
+          className="absolute -right-10 -top-10 w-60 h-60 bg-red-500 rounded-[100%] md:hidden hidden lg:flex"
+        />
+        <p className="absolute lg:top-10 md:top-5 sm:top-6 lg:right-60 md:right-[32%] top-5 right-[25%]  text-4xl font-semibold font-poppins">
+          Mission
+        </p>
+        <p className="absolute lg:ps-[6.4%] lg:top-[48%] md:top-[35%] md:ps-[18.4%] lg:right-60 md:right-[12%] top-[35%] text-lg font-poppins ">
+          To help startups explore new opportunities in new markets and stay
+          thriving!
+        </p>
+      </div>
+
+      <div className=" sm:w-[65%]  mt-10 sm:mt-20 relative items-center sm:h-44 h-52 p-4 bg-[#EBFCFC]  sm:ml-auto">
+        <Image
+          src={Maskgroup}
+          className="absolute -left-10 -top-10 w-60 h-60 bg-red-500 rounded-[100%]  md:hidden hidden lg:flex"
+        />
+        <p className="absolute top-5 lg:left-[39%] md:left-[36%] sm:left-60 left-[25%] text-4xl font-semibold font-poppins">
+          Vision
+        </p>
+        <p className="absolute pe-[6.4%] top-[35%] sm:left-60 lg:left-[40%] md:left-[20%] text-lg font-poppins ">
+          To become a leader that defines a new way to help businesses with 100%
+          success surety.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default VisionMission;
