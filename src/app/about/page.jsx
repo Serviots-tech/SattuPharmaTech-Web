e423b5741d5dfbeb49ddc852bc2cbf55bbf70ba7
harishserviots <harishr@serviots.com>
2025-04-14 15:43:21 +0530

    changes : About Page Responsive fixed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -16,36 +16,45 @@ import { LowerQuote, Quote } from "../constants/svg";
 const About = () => {
   return (
     <>
-      <div className="pt-10 lg:pt-12  px-6 sm:px-10 md:px-16 lg:px-20 xl:px-20 w-full  mx-auto">
+      <div className="pt-10 lg:pt-12 px-6 sm:px-10 md:px-16 lg:px-20 xl:px-20 w-full  mx-auto">
         <p className="text-3xl mb-10 font-bold lg:font-semibold text-[26px] lg:text-[52px] leading-[1.1] font-poppins text-center">
           About SatuPharmTech
         </p>
       </div>
+
       <CardWithImage
         cardImg={docImg?.src}
         cardValue={cardWithFourthImgValue}
         isReversed={false}
       />
 
-      <div className="pt-10 lg:pt-20 px-6 sm:px-10 md:px-16 lg:px-20 xl:px-20 w-full  mx-auto">
-        <p className="text-3xl mb-10 font-semibold font-poppins text-center lg:text-left">
+      {/* <div className="pt-10 lg:pt-28 w-full mx-auto">
+        <p className="text-2xl md:text-4xl mb-10 font-semibold font-poppins text-center lg:text-left px-6 sm:px-10 md:px-16 lg:px-20 xl:px-20">
+          Our Mission & Vision
+        <VisionMission youngConfident={youngConfident} Maskgroup={Maskgroup} />
+        </p>
+      </div> */}
+
+      <div className="pt-10 lg:pt-28 w-full mx-auto">
+        <p className="text-2xl md:text-4xl mb-10 font-semibold font-poppins text-center lg:text-left px-6 sm:px-10 md:px-16 lg:px-20 xl:px-20">
           Our Mission & Vision
         </p>
         <VisionMission youngConfident={youngConfident} Maskgroup={Maskgroup} />
       </div>
+
       <div className="pt-10 lg:pt-20 px-6 sm:px-10 md:px-16 lg:px-20 xl:px-20 w-full  mx-auto">
         <p className="text-2xl leading-[1.2] mb-10 font-bold font-poppins text-center md:text-left">
-        Our Thought Leadership
+          Our Thought Leadership
         </p>
       </div>
       <TeamMembers />
-      
-      <div className="relative mt-20 bg-[#4FB5B9] mb-10 sm:mb-10 md:mb-16 lg:mb-10 xl:mb-10 text-white flex justify-center items-center text-center min-h-[200px]">
+
+      <div className="relative mt-20 bg-[#4FB5B9] mb-10 sm:mb-10 md:mb-16 lg:mb-10 xl:mb-10 text-white flex justify-center items-center text-center min-h-[250px]">
         <span className="absolute left-32 top-5">
           <Quote />
         </span>
         <div className="w-full md:h-60 lg:pt-10 mx-auto">
-          <div className="px-0 sm:px-10 md:px-16 lg:px-20 xl:px-20 w-full max-w-[80%] mx-auto font-poppins text-xl p-10">
+          <div className="px-0 sm:px-10 md:px-16 lg:px-20 xl:px-20 w-full max-w-[80%] mx-auto font-poppins text-xl md:text-xl xl:text-2xl p-10">
             Satu PharmTech stands tall as a leading platform that provides
             comprehensive support, from crop to cup —help establish businesses,
             connect with buyers, conduct research and market products and build
@@ -128,18 +137,18 @@ const About = () => {
         </div>
       </div>
 
-      
 
-    
+
+
       <div className="bg-[#EBFCFC] mt-20 mb-10 md:mb-20 border-t border-b  border-[#4FB5B9] py-10 px-6 md:px-16 flex flex-col items-center text-center">
-        <h2 className="text-2xl md:text-3xl font-semibold text-[#5ABFC3]">
+        <h2 className="text-2xl md:text-3xl font-semibold text-[#5ABFC3] max-w-6xl mx-auto">
           Whether you want to register a new Pharma company in India/Africa,
           start a new venture, or build a custom software solution on top of
           cutting-edge technologies, we can be at your service.
         </h2>
         <Link href="/contact">
           <button className="mt-6 bg-[#4FB5B9] px-20 text-white py-3 rounded-xl text-lg font-medium transition hover:bg-[#48A5A8]">
-            Let’s Connect
+            Let&apos;s Connect
           </button>
         </Link>
       </div>
--- src/components/VisionMission.jsx
+++ src/components/VisionMission.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const VisionMission = ({ youngConfident, Maskgroup }) => {
   return (
     <>
-      <div className=" sm:w-[65%] border-b-2 border-[#FDC446] relative items-center h-60 lg:h-48 p-4 bg-[#FFF7E3] font-poppins mt-[120px] mb-[120px]">
+      <div className="w-[65%] 2xl:w-[60%] border-b-2 border-[#FDC446] relative items-center h-60 lg:h-48 p-4 bg-[#FEEDC6] font-poppins mt-[80px] mb-[120px]">
         <Image
           src={youngConfident}
           // className="absolute -right-14 -top-8 w-60 h-60 bg-red-500 rounded-[100%] md:hidden hidden lg:flex"
@@ -17,16 +17,16 @@ const VisionMission = ({ youngConfident, Maskgroup }) => {
           lg:top-[-2rem] lg:right-[-3.5rem] lg:left-auto lg:translate-x-0
         "
         />
-        <p className="absolute sm:top-18 md:top-[50%] lg:top-10 lg:right-60 md:right-[32%] top-20 right-[25%]  text-4xl font-semibold font-poppins">
+        <p className="absolute sm:top-18 md:top-[50%] lg:top-10 lg:right-60 md:right-[37%] top-24 right-[30%] text-4xl font-semibold font-poppins">
           Mission
         </p>
-        <p className="absolute  lg:text-right lg:ps-[6.4%] lg:top-[48%] md:top-[65%] md:ps-[18.4%] lg:right-60 md:right-[12%] top-[60%] text-lg font-poppins text-center">
+        <p className="absolute lg:text-right lg:ps-[6.4%] lg:top-[48%] md:top-[65%] md:ps-[10.4%] lg:right-60 md:right-[12%] top-[60%] text-lg font-poppins text-center max-w-[600px] mx-auto md:pr-0 pr-2">
           To help startups explore new opportunities in new markets and stay
           thriving!
         </p>
       </div>
 
-      <div className=" sm:w-[65%] border-b-2 border-[#4FB5B9]  mt-10 sm:mt-20 relative items-center h-72 sm:h-64 md:h-64 lg:h-48 p-4 bg-[#EBFCFC]  sm:ml-auto">
+      <div className="w-[65%] 2xl:w-[65%] border-b-2 border-[#4FB5B9] mt-10 sm:mt-20 relative items-center h-72 sm:h-64 md:h-64 lg:h-48 p-4 bg-[#EBFCFC] sm:ml-auto">
         <Image
           src={Maskgroup}
           // className="absolute -left-14 -top-8 w-60 h-60 bg-red-500 rounded-[100%]  md:hidden hidden lg:flex"
@@ -39,14 +39,15 @@ const VisionMission = ({ youngConfident, Maskgroup }) => {
                lg:top-[-2rem] lg:left-[-3.5rem] lg:translate-x-0
                "
         />
-        <p className="absolute top-[30%] sm:top-18 sm:top-[45%] lg:top-[25%] md:top-[45%] lg:left-[36%] md:left-[36%] left-[25%] text-4xl font-semibold font-poppins">
+        <p className="absolute top-24 sm:top-18 sm:top-[45%] lg:top-[25%] md:top-[45%] 2xl:left-[23.5%] lg:left-[29.5%] md:left-[38%] left-[35%] text-4xl font-semibold font-poppins">
           Vision
         </p>
-        <p className="absolute text-center lg:text-left pe-[6.4%] top-[50%] sm:top-[60%] md:top-[60%] lg:top-[50%] lg:left-[36%] md:left-[20%] text-lg font-poppins ">
+        <p className="absolute text-center lg:text-left pe-[6.4%] top-[50%] sm:top-[60%] md:top-[60%] lg:top-[50%] lg:left-[30%] 2xl:left-[24%] md:left-[8%] text-lg font-poppins md:pl-0 sm:pl-2 xl:max-w-xl mx-auto">
           To become a leader that defines a new way to help businesses with 100%
           success surety.
         </p>
       </div>
+
     </>
   );
 };
